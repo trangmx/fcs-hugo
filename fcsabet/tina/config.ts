109a8fbcbdad5d0adefe30aa359173b77173cf6a
Mai Xuan Trang <trang.maixuan@phenikaa-uni.edu.vn>
2023-06-20 13:45:13 +0700

    Add tina

---
+++ fcsabet/tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "0784ebf5-085a-4490-8c27-6c5e123134e4", // Get this from tina.io
+  token: "b01e233349b6e2f2f14fbe5e61fa076ada4ced87", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/english",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
